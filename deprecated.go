b0a2e4bc27bb368e897675e0c9c371ede3166a93
Raúl Kripalani <raul@protocol.ai>
2019-05-08 18:28:33 +0100

    re-alias test utilities; now under go-libp2p-testing.

--- deprecated.go
+++ deprecated.go
@@ -6,90 +6,92 @@ import (
 	"testing"
 
 	ci "github.com/libp2p/go-libp2p-core/crypto"
-	tcrypto "github.com/libp2p/go-libp2p-core/crypto/test"
-	peer "github.com/libp2p/go-libp2p-core/peer"
-	tpeer "github.com/libp2p/go-libp2p-core/peer/test"
-	"github.com/libp2p/go-libp2p-testing/helper"
+	"github.com/libp2p/go-libp2p-core/peer"
+
+	"github.com/libp2p/go-libp2p-testing/crypto"
+	"github.com/libp2p/go-libp2p-testing/etc"
+	"github.com/libp2p/go-libp2p-testing/net"
+	"github.com/libp2p/go-libp2p-testing/peer"
 
 	ma "github.com/multiformats/go-multiaddr"
 )
 
-// Deprecated: use github.com/libp2p/go-libp2p-testing/helper.ZeroLocalTCPAddress instead.
-var ZeroLocalTCPAddress = helper.ZeroLocalTCPAddress
+// Deprecated: use github.com/libp2p/go-libp2p-testing/net.ZeroLocalTCPAddress instead.
+var ZeroLocalTCPAddress = tnet.ZeroLocalTCPAddress
 
-// Deprecated: use github.com/libp2p/go-libp2p-core/crypto/test.RandTestKeyPair instead.
+// Deprecated: use github.com/libp2p/go-libp2p-testing/crypto.RandTestKeyPair instead.
 // Supply RSA as a key type to get an equivalent result.
 func RandTestKeyPair(bits int) (ci.PrivKey, ci.PubKey, error) {
 	return tcrypto.RandTestKeyPair(ci.RSA, bits)
 }
 
-// Deprecated: use github.com/libp2p/go-libp2p-core/crypto/test.SeededTestKeyPair instead.
+// Deprecated: use github.com/libp2p/go-libp2p-testing/crypto.SeededTestKeyPair instead.
 // Supply RSA as a key type, with 512 bits, to get an equivalent result.
 func SeededTestKeyPair(seed int64) (ci.PrivKey, ci.PubKey, error) {
 	return tcrypto.SeededTestKeyPair(ci.RSA, 512, seed)
 }
 
-// Deprecated: use github.com/libp2p/go-libp2p-core/peer/test.RandPeerID instead.
+// Deprecated: use github.com/libp2p/go-libp2p-testing/peer.RandPeerID instead.
 func RandPeerID() (peer.ID, error) {
 	return tpeer.RandPeerID()
 }
 
-// Deprecated: use github.com/libp2p/go-libp2p-core/peer/test.RandPeerIDFatal instead.
+// Deprecated: use github.com/libp2p/go-libp2p-testing/peer.RandPeerIDFatal instead.
 func RandPeerIDFatal(t testing.TB) peer.ID {
 	return tpeer.RandPeerIDFatal(t)
 }
 
-// Deprecated: use github.com/libp2p/go-libp2p-testing/helper.RandLocalTCPAddress instead.
+// Deprecated: use github.com/libp2p/go-libp2p-testing/net.RandLocalTCPAddress instead.
 func RandLocalTCPAddress() ma.Multiaddr {
-	return helper.RandLocalTCPAddress()
+	return tnet.RandLocalTCPAddress()
 }
 
-// Deprecated: use github.com/libp2p/go-libp2p-testing/helper.PeerNetParams instead.
-type PeerNetParams = helper.PeerNetParams
+// Deprecated: use github.com/libp2p/go-libp2p-testing/net.PeerNetParams instead.
+type PeerNetParams = tnet.PeerNetParams
 
-// Deprecated: use github.com/libp2p/go-libp2p-testing/helper.RandPeerNetParamsOrFatal instead.
+// Deprecated: use github.com/libp2p/go-libp2p-testing/net.RandPeerNetParamsOrFatal instead.
 func RandPeerNetParamsOrFatal(t *testing.T) PeerNetParams {
-	return helper.RandPeerNetParamsOrFatal(t)
+	return tnet.RandPeerNetParamsOrFatal(t)
 }
 
-// Deprecated: use github.com/libp2p/go-libp2p-testing/helper.RandPeerNetParams instead.
+// Deprecated: use github.com/libp2p/go-libp2p-testing/net.RandPeerNetParams instead.
 func RandPeerNetParams() (*PeerNetParams, error) {
-	return helper.RandPeerNetParams()
+	return tnet.RandPeerNetParams()
 }
 
-// Deprecated: use github.com/libp2p/go-libp2p-testing/helper.Identity instead.
-type Identity = helper.Identity
+// Deprecated: use github.com/libp2p/go-libp2p-testing/net.Identity instead.
+type Identity = tnet.Identity
 
-// Deprecated: use github.com/libp2p/go-libp2p-testing/helper.RandIdentity instead.
+// Deprecated: use github.com/libp2p/go-libp2p-testing/net.RandIdentity instead.
 func RandIdentity() (Identity, error) {
-	return helper.RandIdentity()
+	return tnet.RandIdentity()
 }
 
-// Deprecated: use github.com/libp2p/go-libp2p-testing/helper.RandIdentityOrFatal instead.
+// Deprecated: use github.com/libp2p/go-libp2p-testing/net.RandIdentityOrFatal instead.
 func RandIdentityOrFatal(t *testing.T) Identity {
-	return helper.RandIdentityOrFatal(t)
+	return tnet.RandIdentityOrFatal(t)
 }
 
-// Deprecated: use github.com/libp2p/go-libp2p-testing/helper.NewIdentity instead.
+// Deprecated: use github.com/libp2p/go-libp2p-testing/net.NewIdentity instead.
 func NewIdentity(ID peer.ID, addr ma.Multiaddr, privk ci.PrivKey, pubk ci.PubKey) Identity {
-	return helper.NewIdentity(ID, addr, privk, pubk)
+	return tnet.NewIdentity(ID, addr, privk, pubk)
 }
 
-// Deprecated: use github.com/libp2p/go-libp2p-testing/helper.LatencyConfig instead.
-type LatencyConfig = helper.LatencyConfig
+// Deprecated: use github.com/libp2p/go-libp2p-testing/net.LatencyConfig instead.
+type LatencyConfig = tnet.LatencyConfig
 
-// Deprecated: use github.com/libp2p/go-libp2p-testing/helper.SeededRand instead.
-var SeededRand = helper.SeededRand
+// Deprecated: use github.com/libp2p/go-libp2p-testing/etc.SeededRand instead.
+var SeededRand = tetc.SeededRand
 
-// Deprecated: use github.com/libp2p/go-libp2p-testing/helper.NewSeededRand instead.
+// Deprecated: use github.com/libp2p/go-libp2p-testing/etc.NewSeededRand instead.
 func NewSeededRand(seed int64) *rand.Rand {
-	return helper.NewSeededRand(seed)
+	return tetc.NewSeededRand(seed)
 }
 
-// Deprecated: use github.com/libp2p/go-libp2p-testing/helper.LockedRandSource instead.
-type LockedRandSource = helper.LockedRandSource
+// Deprecated: use github.com/libp2p/go-libp2p-testing/etc.LockedRandSource instead.
+type LockedRandSource = tetc.LockedRandSource
 
-// Deprecated: use github.com/libp2p/go-libp2p-testing/helper.WaitFor instead.
+// Deprecated: use github.com/libp2p/go-libp2p-testing/etc.WaitFor instead.
 func WaitFor(ctx context.Context, check func() error) error {
-	return helper.WaitFor(ctx, check)
+	return tetc.WaitFor(ctx, check)
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/libp2p/go-testutil
 
 require (
 	github.com/libp2p/go-libp2p-core v0.0.0-20190508144953-ed42958fbb3a
-	github.com/libp2p/go-libp2p-testing v0.0.0-20190508145120-25bb910661fd
+	github.com/libp2p/go-libp2p-testing v0.0.0-20190508172549-1a0da3de1915
 	github.com/multiformats/go-multiaddr v0.0.2
 	golang.org/x/sys v0.0.0-20190222072716-a9d3bda3a223 // indirect
 )
